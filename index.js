bf9c5a1b94576099564cadc01515af97ee95b409
L <reneelsq@hotmail.com>
2019-02-14 16:10:17 +0100

    nodejs file finished

--- index.js
+++ index.js
@@ -1,5 +1,30 @@
 #!/usr/bin/env node
 
+//console.log("Hello, Node.JS!");
+
+const validator = require("email-validator");
+const axios = require("axios");
+const chalk = require("chalk");
+//to get the email adress
+const askEmail = require("readline-sync");
+const emailInput = askEmail.question("Please enter your email: ");
 
 
-console.log("Hello, Node.JS!");
+
+//style
+const correctMessage = chalk.green.bgWhite;
+const wrongMessage = chalk.red.bgWhite;
+
+
+if(validator.validate(emailInput)){ 
+    const encodeEmail = encodeURIComponent(emailInput);
+    const url = "https://haveibeenpwned.com/api/v2/breachedaccount/"+encodeEmail;
+
+    axios.get(url,{"headers":{"user-agent":"Node_CLI tool"}})
+    .then(function(response){
+        console.log(correctMessage("pwnage found!"));
+    })
+    .catch(function(error){
+        console.log(wrongMessage("no pwnage found!"));
+    });
+}
