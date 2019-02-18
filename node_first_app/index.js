524ae141c025c85aea19bd92e76eb8695e3e4d62
L <reneelsq@hotmail.com>
2019-02-18 16:50:13 +0100

    files updated

--- node_first_app/index.js
+++ node_first_app/index.js
@@ -1,17 +1,29 @@
 //import the NPM dependancy package
 const express = require("express");
-const bodyParser = require("body-parser");
+
 //initialise express() inside and write to the app variable
 const app = express();
+
+const bodyParser = require("body-parser");
 //import route module and pass your app
-require("./routes/userRoutes")(app);
+//require("./routes/userRoutes")(app);
+
 //choose what port on which to run the server
 const PORT = 5000;
 
  //parse body of incoming json requests
- //app.use(bodyParser.json());
+ app.use(bodyParser.json());
 //use the app variable and listen on the port
-app.listen(PORT, () => {
-   
+app.get("/users", (req, res) => {
+
+    let users;
+
+    // request data from server and save in cars variable;
+
+    res.send("users")
+  })
+
+
+app.listen(PORT, () => {   
     console.log(`Server running`);
 });
--- node_first_app/routes/userRoutes.js
+++ node_first_app/routes/userRoutes.js
@@ -4,66 +4,14 @@
 // You will notice when we use this module that app is given as a variable
 // The technical term for this is currying
 
-module.exports = app => {
+/* module.exports = app => {
+    app.get("/users", (req, res) => {
+
+        let users;
     
-    var jsonfile = require("jsonfile")
-    //var file = "/tmp/data.json"
-    //var obj = {name: "JP"}
-app.use(bodyParser.json());
-
+        // request data from server and save in cars variable;
     
-    app.get("/users",(req,res)=>{
-        console.log("fetching all users");
-        //jsonfile reading
-       
-        jsonfile.readFile("./DB/users.json", function(err, content){
-            //send file contents back to sender
-            res.json(content);
-        });
-    });
-    
+        res.send(users)
+      })
 
-   /*  jsonfile.writeFile(file,obj,function(err){
-        console.error(err)
-    }) */
-
-
-    
-     //app.get exercise
-    // Route is accessible as GET: /demo
-    // req is the parameter that refers to the request data
-    // res is the parameter that refers to the request's sender
-    //
-  
-   /*  app.get("/users", (req, res) => {
-        // Some dummy data
-      //
-        console.log("fetching all users");
-         // res.send(data) or res.sendStatus(statusCode)
-      // To send a status message or data back to the requester
-      //
-        //res.send(users);
-
-        //jsonfile reading
-        jsonfile.readFile("./DB/users.json", function(err,content){
-            //send file contents back to sender
-            res.send(content);
-        });    
-    }) */
-
-    //app.post exercise
-    /* app.post("/users/new",(req,res) => {
-        let email = req.body.email
-        let username = req.body.username
-        jsonfile.readFile("./DB/users.json", function(err, content){
-            content.push({email:email, username: username});
-            console.log("added" + email +"to DB");
-            jsonfile.writeFile("./DB/users.json", content, function(err){
-                console.log(err);
-            });
-            res.sendStatus(200);
-        });
-    }); */
-   
-
-  };
+ }; */
